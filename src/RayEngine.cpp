054186d8c6c2a02f70965f20f6792ffd193cb15f
Atzubi <lolmaunz@googlemail.com>
2019-11-13 17:26:12 +0100

    Revert "code refactoring"
    This reverts commit c9fb1601517f059f110db3be63c7b7959e0d465b.

--- src/RayEngine.cpp
+++ src/RayEngine.cpp
@@ -34,15 +34,47 @@ bool RayEngine::bindGeometryToPipeline(int pipelineId, std::vector<int>* objectI
     return false;
 }
 
-int RayEngine::addObject(Object const &object, Vector3D position, Vector3D orientation, double newScaleFactor,
-                      RayEngine::ObjectParameter objectParameter) {
-    return 0;
-}
-
 bool RayEngine::removeObject(int id) {
     return false;
+}
+
+bool RayEngine::moveObject(int id, Vector3D newPosition) {
+    return false;
+}
+
+bool RayEngine::turnObject(int id, Vector3D newOrientation) {
+    return false;
+}
+
+int RayEngine::addObject(Object const &object, Vector3D position, Vector3D orientation, double newScaleFactor,
+                         RayEngine::ObjectParameter objectParameter) {
+    return 0;
 }
 
 bool RayEngine::updateObject(int id, Object const &object) {
     return false;
 }
+
+bool RayEngine::scaleObject(int id, double newScaleFactor) {
+    return false;
+}
+
+bool RayEngine::manipulateObject(int id, Vector3D newPosition, Vector3D newOrientation, double newScaleFactor) {
+    return false;
+}
+
+Object::Object(std::vector<double>* const vertices, std::vector<double>* const normals, std::vector<double>* const map,
+               std::vector<uint64_t>* const ids) : vertices(vertices), normals(normals), map(map), ids(ids) {
+    if(ids->size() %3 != 0){
+        std::__throw_invalid_argument("Invalid ID Count");
+    }
+    if(vertices->size() != normals->size()){
+        if(!normals->empty()){
+            std::__throw_invalid_argument("Invalid Normal Count");
+        }
+    }else if(vertices->size() != map->size()){
+        std::__throw_invalid_argument("Invalid Texture Coordinate Count");
+    }
+}
+
+Object::~Object() = default;
