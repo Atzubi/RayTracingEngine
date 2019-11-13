c88feff3c1727ee02588cda16beeecd3e4309a63
Atzubi <lolmaunz@googlemail.com>
2019-11-13 17:41:35 +0100

    code refactoring

---
+++ API/BasicStructures.h
@@ -0,0 +1,17 @@
+//
+// Created by sebastian on 13.11.19.
+//
+
+#ifndef RAYTRACECORE_BASICSTRUCTURES_H
+#define RAYTRACECORE_BASICSTRUCTURES_H
+
+/**
+ * Contains x, y and z coordinates representing a vector in 3 dimensions
+ */
+struct Vector3D{
+    double x;
+    double y;
+    double z;
+};
+
+#endif //RAYTRACECORE_BASICSTRUCTURES_H
